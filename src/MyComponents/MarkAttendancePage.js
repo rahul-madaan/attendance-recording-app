8dbd69224db6e73d51130bca75cebecbdf931274
Rahul Madan <madan.rahul9@gmail.com>
2022-10-30 23:44:35 +0530

    added button to upload photo

--- src/MyComponents/MarkAttendancePage.js
+++ src/MyComponents/MarkAttendancePage.js
@@ -7,6 +7,9 @@ import axios from "axios";
 import 'react-toastify/dist/ReactToastify.css'
 import {toast} from "react-toastify";
 import {getCurrentBrowserFingerPrint} from "@rajesh896/broprint.js";
+import Button from '@material-ui/core/Button';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import IconButton from '@material-ui/core/IconButton';
 
 export const MarkAttendancePage = (props) => {
 
@@ -239,6 +242,36 @@ export const MarkAttendancePage = (props) => {
             <br/>
             <br/>
             <br/>
+            <div style={{
+                display: 'flex',
+                margin: 'auto',
+                width: 400,
+                flexWrap: 'wrap',
+            }}>
+                <div style={{ width: '100%', float: 'left' }}>
+                    <h3>How to use create button to choose file in ReactJS?</h3> <br />
+                </div>
+                <input
+                    type="file"
+                    accept="image/*"
+                    style={{ display: 'none' }}
+                    id="contained-button-file"
+                />
+                <label htmlFor="contained-button-file">
+                    <Button variant="contained" color="primary" component="span">
+                        Upload
+                    </Button>
+                </label>
+                <h3>  OR  </h3>
+                <input accept="image/*" id="icon-button-file"
+                       type="file" style={{ display: 'none' }} />
+                <label htmlFor="icon-button-file">
+                    <IconButton color="primary" aria-label="upload picture"
+                                component="span">
+                        <PhotoCamera />
+                    </IconButton>
+                </label>
+            </div>
 
         </>
     )
