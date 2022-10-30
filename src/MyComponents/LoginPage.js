1e16ba7dd2f24f72ac31612e7812645684492971
Rahul Madan <madan.rahul9@gmail.com>
2022-10-30 13:21:37 +0530

    updated login button loading and notification on incorrect pass

--- src/MyComponents/LoginPage.js
+++ src/MyComponents/LoginPage.js
@@ -7,6 +7,7 @@ import {
 }
     from 'mdb-react-ui-kit';
 import {toast} from "react-toastify";
+import {useState} from "react";
 
 
 export const LoginPage = (props) => {
@@ -14,13 +15,17 @@ export const LoginPage = (props) => {
     const routeChange = (path) => {
         navigate(path);
     }
+    const [loginButtonDisabled, setLoginButtonDisabled] = useState(false)
+
 
     const loginSubmit = (e) => {
         e.preventDefault()
+        setLoginButtonDisabled(true)
         axios.post(process.env.REACT_APP_API_URI + process.env.REACT_APP_API_VERSION + "/login", {
             'net_id': props.userSNUID,
             'password': props.loginPassword
         }).then((result) => {
+            setLoginButtonDisabled(false)
             if (result.data.status === "LOGIN_SUCCESSFUL") {
                 localStorage.clear()
                 localStorage.setItem("user_net_id", result.data.encrypted_net_id)
@@ -87,11 +92,15 @@ export const LoginPage = (props) => {
                             }} className="form-control" placeholder={"Enter Your Roll Number"}/>
                         </div>
                         <div className="text-center mb-3 ">
+                            {loginButtonDisabled?<button className="btn btn-primary my-3 w-75" type="button" disabled>
+                                <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"/>
+                                Logging in
+                            </button>:
                             <button type="submit" className="btn btn-primary my-3 w-75" onClick={loginSubmit}
                                     onKeyPress={(e) => {
                                 console.log(e.key)
                             }}>Log in
-                            </button>
+                            </button>}
                         </div>
 
 
