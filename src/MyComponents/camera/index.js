34346800047c704a09734dd76aab61f7d9c98105
Rahul Madan <madan.rahul9@gmail.com>
2022-11-02 21:44:39 +0530

    updated requested camera to user facing camera from environment facing camera

--- src/MyComponents/camera/index.js
+++ src/MyComponents/camera/index.js
@@ -15,7 +15,7 @@ import {
 
 const CAPTURE_OPTIONS = {
     audio: false,
-    video: { facingMode: "environment" }
+    video: { facingMode: "user" }
 };
 
 export function Camera({ onCapture, onClear }) {
