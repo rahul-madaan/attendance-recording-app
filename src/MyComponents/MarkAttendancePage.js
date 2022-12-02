b323e7139f50b49d138341dcbbc6722a7d1c08a6
Rahul Madan <madan.rahul9@gmail.com>
2022-12-03 01:36:56 +0530

    added feature of multiple face detection

--- src/MyComponents/MarkAttendancePage.js
+++ src/MyComponents/MarkAttendancePage.js
@@ -12,27 +12,27 @@ import {toast} from "react-toastify";
 export const MarkAttendancePage = (props) => {
 
     const [name, setName] = useState("")
-    const [rollNumber, setRollNumber] = useState("")
-    const [collectUserDetailsLoading, setCollectUserDetailsLoading] = useState(false)
-    const [collectUserDetailsDisabled, setCollectUserDetailsDisabled] = useState(true)
-    const [markAttendanceDisabled, setMarkAttendanceDisabled] = useState(true)
+    const [isLoading, setIsLoading] = useState(false)
     const [isCameraOpen, setIsCameraOpen] = useState(false);
     const [cardImage, setCardImage] = useState();
 
     const UploadButtonClick = async(e) => {
         e.preventDefault()
+        setIsLoading(true)
         const formData = new FormData();
         formData.append("file", cardImage);
         console.log(formData)
         try {
             const response = await axios({
                 method: "post",
-                url: process.env.REACT_APP_API_URI + process.env.REACT_APP_API_VERSION + "/file",
+                url: process.env.REACT_APP_API_URI + process.env.REACT_APP_API_VERSION + "/detect-stored-faces",
                 data: formData,
                 headers: { "Content-Type": "multipart/form-data" },
             });
             console.log(formData)
             console.log(response)
+            setName(response.data)
+            setIsLoading(false)
         } catch(error) {
             console.log(error)
         }
@@ -41,9 +41,20 @@ export const MarkAttendancePage = (props) => {
 
 
     return (<>
+            {!isLoading?
+            <div className="container  d-flex justify-content-center">
+                <button className="btn my-3 btn-lg btn-success" type="button" onClick={UploadButtonClick}>
+                    Check Face
+                </button>
+            </div>:
+            <div className="container  d-flex justify-content-center">
+                <button className="btn my-3 btn-lg btn-success" type="button" disabled>
+                    <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"/>
+                    Processing...
+                </button>
+            </div>}
 
-            <button type="button" className="btn btn-success btn-lg my-3 mx-3" onClick={UploadButtonClick}>UPLOAD</button>
-
+            <p>DETECTED FACE: {name}</p>
             <Fragment>
                 <Root>
                     {isCameraOpen && (
@@ -74,7 +85,6 @@ export const MarkAttendancePage = (props) => {
                     </Footer>
                 </Root>
             </Fragment>
-            <p>{console.log(cardImage)}</p>
         </>
     )
 }
